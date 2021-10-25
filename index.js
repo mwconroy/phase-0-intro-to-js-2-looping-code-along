1b4d8c1d5b0fa2d8c6ebc1e4ec0ddbec58397ddc
Mike <mikewconroy3@gmail.com>
2021-10-24 21:33:04 -0500

    Got stuck, cant get it to log

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = ["Lisa", "Kaitlyn", "Ali"];
+
+function writeCards(names, event) {
+  let birthday =[];
+  event = "surprise";
+  for (let i= 0; i < names.length; i++) {
+  console.log(birthday[i] = 'Thank you, '+ names[i] +', for the wonderful ' +event+' gift!');
+  debugger
+  }
+  return birthday;
+}
+
+let number = 10;
+function countdown (number){} 
+  while (number >= 0) {
+    console.log(number--);
+}
